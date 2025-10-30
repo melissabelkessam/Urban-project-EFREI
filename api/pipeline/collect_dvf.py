e4c600c8d5e796f69b1bdae94d4bc4d3766eaf43
Melissa Belkessam <melissabelkessmabelkessam@gmail.com>
2025-10-30 10:41:33 +0100

    melissaaafffffddddf

--- api/pipeline/collect_dvf.py
+++ api/pipeline/collect_dvf.py
@@ -1,37 +1,32 @@
 import requests
 from pathlib import Path
-from dotenv import load_dotenv
-import os
 
-# Charger les variables du fichier .env
-load_dotenv()
-
-# Lire les variables d'environnement
-BASE_URL = os.getenv("DVF_API_BASE")
-CODE_DEPT = os.getenv("DVF_CODE_DEPT")
-
-# Construire l'URL complète de l'API DVF
-url = f"{BASE_URL}valeursfoncieres-2025-s1.txt.zip"
-
-
-
-
-# Créer le dossier Bronze s'il n'existe pas
+# === Étape 1 : définir le dossier de destination ===
 bronze_path = Path(__file__).resolve().parents[2] / "data" / "Bronze"
 bronze_path.mkdir(parents=True, exist_ok=True)
 
-# Définir le chemin du fichier de sortie
-output_file = bronze_path / f"dvf_{CODE_DEPT}.zip"
+# === Étape 2 : dictionnaire contenant les liens DVF officiels ===
+urls = {
+    2022: "https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20251018-234844/valeursfoncieres-2022.txt.zip",
+    2023: "https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20251018-234851/valeursfoncieres-2023.txt.zip",
+    2024: "https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20251018-234857/valeursfoncieres-2024.txt.zip",
+    2025: "https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20251018-234902/valeursfoncieres-2025-s1.txt.zip",
+}
 
+# === Étape 3 : téléchargement automatique ===
+for year, url in urls.items():
+    output_file = bronze_path / f"dvf_75_{year}.zip"
+    print(f"⏳ Téléchargement DVF {year} ...")
 
-# Télécharger le fichier CSV depuis l'API
-print("⏳ Téléchargement des données DVF en cours...")
-response = requests.get(url)
+    try:
+        response = requests.get(url, timeout=60)
+        if response.status_code == 200:
+            with open(output_file, "wb") as f:
+                f.write(response.content)
+            print(f"✅ Fichier {year} enregistré dans {output_file}")
+        else:
+            print(f"⚠️ Erreur {response.status_code} pour {year}")
+    except Exception as e:
+        print(f"❌ Erreur {year} :", e)
 
-# Vérifier si la requête a réussi
-if response.status_code == 200:
-    with open(output_file, "wb") as f:
-        f.write(response.content)
-    print(f"✅ Données DVF sauvegardées dans : {output_file}")
-else:
-    print(f"❌ Erreur {response.status_code} lors du téléchargement.")
+print("\n🏁 Téléchargement DVF terminé avec succès !")
